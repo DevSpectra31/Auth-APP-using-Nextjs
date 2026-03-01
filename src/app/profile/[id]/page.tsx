40bfb9ce0c6ca407298b9e009319e385ed27abea
Er. Vineet Rajpal <rajpalvineet800@gmail.com>
2026-03-01 19:17:23 +0530

    params.id is coming undefined

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,21 +1,13 @@
-interface UserProfileProps {
-  params: {
-    id: string;
-  };
-}
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export default function ProfilePage({ params }: any ) {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Profile</h1>
+            <hr />
+            <p className="text-4xl">Profile Page
 
-export default function UserProfile({ params }: UserProfileProps) {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-xl font-semibold">Profile</h1>
-      <hr className="w-40 my-4 border-gray-700" />
-
-      <div className="flex items-center gap-3 text-4xl font-bold">
-        <span>Profile Page</span>
-        <span className="px-4 py-2 rounded-lg bg-orange-500 text-white text-2xl">
-          {params.id}
-        </span>
-      </div>
-    </div>
-  );
+                <span className=" p-2 rounded bg-orange-500 text-black">{params.id}</span>
+            </p>
+        </div>
+    )
 }
