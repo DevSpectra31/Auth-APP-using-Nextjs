37f019b82ea6bfbedf10f15883cb5d80bc1da5e9
Er. Vineet Rajpal <rajpalvineet800@gmail.com>
2026-03-02 17:53:52 +0530

    params.id is redndering on the profile/[id]/page.tsx -> params is promise should be wrapped in await .

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import React, {useState} from "react";
+import {toast} from "react-hot-toast";
+import {useRouter} from "next/navigation";
+
+
+export default function ProfilePage() {
+    const router = useRouter()
+    const [data, setData] = useState("nothing")
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Profile</h1>
+            <hr />
+            <p>Profile page</p>
+            <h2 className="p-1 rounded bg-green-500">{data === 'nothing' ? "Nothing" : <Link href={`/profile/${data}`}>{data}
+            </Link>}</h2>
+        <hr />
+            </div>
+    )
+}
